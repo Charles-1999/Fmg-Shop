58387cfcf2163d8ec1fa3d913c1bb365cf2767e9
Charles_Luk <792281365@qq.com>
2020-10-26 15:45:38 +0800

    添加页面：研学（课程、资讯）

--- src/app.config.js
+++ src/app.config.js
@@ -18,6 +18,9 @@ export default {
     'pages/user/Order/orderDetail',
     'pages/user/Order/myOrder',
     'pages/user/Order/deliveryDetail',
+    'pages/studies/index',
+    'pages/studies/course/index',
+    'pages/studies/news/index',
   ],
   window: {
     backgroundTextStyle: 'light',
